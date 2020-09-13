31da90ce4a0ddf3f286c895f962e4bce74d5410f
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-09-12 21:48:49 -0500

    Created new ocmponent and added style

--- src/App.js
+++ src/App.js
@@ -3,45 +3,20 @@ import './App.css'
 
 class Colorized extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: '',
-      bgColor: 'white'
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({
-      color: e.target.value
-    })
-  }
-
-  handleSubmit(e) {
-    this.setState({
-      bgColor: this.state.color
-    })
-    e.preventDefault();
-  }
-
   render() {
-    var boxColor = {
+    var styleBg = {
       backgroundColor: this.state.bgColor
-    };
+    }
     return (
-      <div className="main">
-        <div style={boxColor} className="colorbox"></div>
+      <div>
+        <div style={styleBg} className="colorBox"></div>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange}
-            placeholder="entenr a color">
-
+            placeholder="enter a color">
           </input>
           <button type="submit">go</button>
         </form>
-      </div >
+      </div>
     )
   }
 }
